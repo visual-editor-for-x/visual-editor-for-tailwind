9097073351de15abf9017d8ead8518fe121b403b
Ryohei Ikegami <iofg2100@gmail.com>
2022-07-29 18:29:57 +0900

    Use paintkit component

--- packages/tailwind-inspect/src/App.tsx
+++ packages/tailwind-inspect/src/App.tsx
@@ -1,33 +1,38 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Button } from "@seanchas116/paintkit/src/components/Button";
+import { PaintkitRoot } from "@seanchas116/paintkit/src/components/PaintkitRoot";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App text-red-500">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <PaintkitRoot colorScheme="auto">
+      <div className="App text-red-500">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
+        <Button>Button</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </PaintkitRoot>
   );
 }
 
