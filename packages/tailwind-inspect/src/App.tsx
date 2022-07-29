bb6ab02ff591749ee91608c1fcd1d92b9f00ff4e
Ryohei Ikegami <iofg2100@gmail.com>
2022-07-29 21:48:35 +0900

    Basic layout

--- packages/tailwind-inspect/src/App.tsx
+++ packages/tailwind-inspect/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Input } from "@seanchas116/paintkit/src/components/Input";
 import { PaintkitRoot } from "@seanchas116/paintkit/src/components/PaintkitRoot";
 
 function App() {
@@ -9,28 +7,9 @@ function App() {
 
   return (
     <PaintkitRoot colorScheme="auto">
-      <div className="text-red-500 flex flex-col items-center">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        <Input className="w-20" />
+      <div className="flex w-full h-full fixed left-0 top-0">
+        <div className="flex-1"></div>
+        <div className="bg-gray-500 w-64"></div>
       </div>
     </PaintkitRoot>
   );
