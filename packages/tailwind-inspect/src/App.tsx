273b94d750204a4bae795d3f4da122f062a44ce1
Ryohei Ikegami <iofg2100@gmail.com>
2022-07-30 00:13:54 +0900

    Fix & Instantiate

--- packages/tailwind-inspect/src/App.tsx
+++ packages/tailwind-inspect/src/App.tsx
@@ -1,11 +1,14 @@
 import { PaintkitRoot } from "@seanchas116/paintkit/src/components/PaintkitRoot";
+import { StyleInspector } from "./inspector/StyleInspector";
 
 function App() {
   return (
     <PaintkitRoot colorScheme="auto">
       <div className="flex w-full h-full fixed left-0 top-0">
         <div className="flex-1"></div>
-        <div className="bg-gray-500 w-64"></div>
+        <div className="bg-gray-500 w-64">
+          <StyleInspector />
+        </div>
       </div>
     </PaintkitRoot>
   );
