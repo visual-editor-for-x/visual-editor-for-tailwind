4aa70c0db4c7deb24fd98540186b0d94a963e732
Ryohei Ikegami <iofg2100@gmail.com>
2022-08-05 23:46:42 +0900

    Update test

--- packages/tailwind-inspect/src/models/Style.test.ts
+++ packages/tailwind-inspect/src/models/Style.test.ts
@@ -5,9 +5,13 @@ describe(Style.name, () => {
   describe("loadTailwind", () => {
     it("should load tailwind classnames", () => {
       const style = new Style();
-      style.loadTailwind("bg-[#fff]");
+      style.loadTailwind("bg-[#fff] absolute font-['Open_Sans']");
       expect(style.background).toEqual("#fff");
-      expect(style.toTailwind()).toEqual("bg-[#fff]");
+      expect(style.position).toEqual("absolute");
+      expect(style.fontFamily).toEqual("Open Sans");
+      expect(style.toTailwind()).toEqual(
+        "absolute font-['Open_Sans'] bg-[#fff]"
+      );
     });
   });
 });
