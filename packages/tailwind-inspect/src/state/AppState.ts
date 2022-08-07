babae72a67961759951d96d2c21ee9957a8cfc4d
Ryohei Ikegami <iofg2100@gmail.com>
2022-08-07 16:40:21 +0900

    Test generating code

--- packages/tailwind-inspect/src/state/AppState.ts
+++ packages/tailwind-inspect/src/state/AppState.ts
@@ -2,6 +2,7 @@ import { ElementInstance } from "../models/ElementInstance";
 import { StyleInspectorState } from "./StyleInspectorState";
 
 import { parse } from "@babel/parser";
+import generate from "@babel/generator";
 import demoCode from "./demo?raw";
 import { transform } from "@babel/standalone";
 import { makeObservable, observable } from "mobx";
@@ -16,6 +17,12 @@ export class AppState {
     });
     console.log(demoCode);
     console.log(parsed);
+
+    parsed.program.body.reverse();
+
+    const { code } = generate(parsed, {}, demoCode);
+
+    console.log(code);
 
     const output = transform(demoCode, { presets: ["env", "react"] }).code;
     console.log(output);
--- packages/tailwind-inspect/vite.config.ts
+++ packages/tailwind-inspect/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,19 @@ export default defineConfig({
   resolve: {
     alias: {
       path: "path-browserify",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
     },
   },
 });
