65b23fd2ac42e10ab9a3aeaae9989c59ca59168a
Ryohei Ikegami <iofg2100@gmail.com>
2022-08-10 23:00:52 +0900

    Debug: Set window.getInstanceFromNode

--- packages/tailwind-inspect/src/main.tsx
+++ packages/tailwind-inspect/src/main.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// @ts-ignore
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom";
 import App from "./App";
 import "./index.css";
+
+// @ts-ignore
+window.getInstanceFromNode =
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.Events[0];
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
