23079780d0d9b1fb49c4e1707c0f1a27c712dc5d
Ryohei Ikegami <iofg2100@gmail.com>
2022-08-11 19:20:27 +0900

    Get source location of clicked node

--- packages/tailwind-inspect/src/views/App.tsx
+++ packages/tailwind-inspect/src/views/App.tsx
@@ -1,9 +1,15 @@
 import { PaintkitRoot } from "@seanchas116/paintkit/src/components/PaintkitRoot";
 import { observer } from "mobx-react-lite";
 import React from "react";
+// @ts-ignore
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom";
 import { StyleInspector } from "../inspector/StyleInspector";
 import { JSXTreeView } from "../outline/JSXTreeView";
 import { AppState } from "../state/AppState";
+
+// @ts-ignore
+const getInstanceFromNode =
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.Events[0];
 
 const appState = new AppState();
 
@@ -47,7 +53,25 @@ const DemoRunner = observer(({ appState }: { appState: AppState }) => {
 
   const Component = exports.default!;
 
-  return <Component />;
+  const onClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const element = e.target as HTMLElement;
+
+    const fiberNode = getInstanceFromNode(element);
+    const debugSource: SourceLocation | undefined = fiberNode?._debugSource;
+    console.log(debugSource);
+  };
+
+  return (
+    <div onClick={onClick}>
+      <Component />
+    </div>
+  );
 });
+
+interface SourceLocation {
+  fileName: string;
+  lineNumber: number;
+  columnNumber: number;
+}
 
 export default App;
