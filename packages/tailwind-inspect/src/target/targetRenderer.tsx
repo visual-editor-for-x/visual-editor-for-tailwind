8dc13d585ace22b5def2b2c91f6c3d456b7ec263
Ryohei Ikegami <iofg2100@gmail.com>
2022-08-24 18:43:38 +0900

    Fix

--- packages/tailwind-inspect/src/target/targetRenderer.tsx
+++ packages/tailwind-inspect/src/target/targetRenderer.tsx
@@ -5,8 +5,21 @@ import Target from "./target";
 const root = document.createElement("div");
 document.body.appendChild(root);
 
-ReactDOM.createRoot(root).render(
+const reactRoot = ReactDOM.createRoot(root);
+
+reactRoot.render(
   <React.StrictMode>
     <Target />
   </React.StrictMode>
 );
+
+if (import.meta.hot) {
+  import.meta.hot.accept("./target", () => {
+    console.log("accept");
+    reactRoot.render(
+      <React.StrictMode>
+        <Target />
+      </React.StrictMode>
+    );
+  });
+}
